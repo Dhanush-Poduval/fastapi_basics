3d802dea27b8666a974baee85c717afa9e3deb73
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-07-28 17:30:45 +0530

    feat:learnt the get command and the path parameter

---
+++ myapi.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+app=FastAPI()
+students={
+    1:{
+        "name":"john",
+        "age":17,
+        "class":"year 12",
+    }
+}
+@app.get("/")
+def index():
+    return{"Name":"First Data"}
+@app.get("/get-student/{student_id}")
+def get_student(student_id:int): 
+    return students[student_id]
+
